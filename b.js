65b9a2926a440dd8faff08e42d3399bc97d70f3f
Sergey Litvinov <slitvinov@gmail.com>
2020-06-24 12:34:39 +0200

    resize better

--- b.js
+++ b.js
@@ -4,20 +4,26 @@ https://twitter.com/Hau_kun/status/1275084507473985539
 */
 
 var t = 0
-var w = 700
-
 function setup() {
-  createCanvas(w, w)
-  stroke('white')
+    var cnv
+    cnv = createCanvas(windowWidth, windowHeight)
+    cnv.style('display', 'block')
+    stroke('black')
+    strokeWeight(2)
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
 function draw() {
-  background('black')
-  t += 0.009
-  for (i = 0; i < 6; i+=PI/4)
-    f(w/2, w/2, i, w/7)
+    var L
+    L = min(height, width)
+    background('white')
+    t += 0.009
+    for (i = 0; i < 6; i+=PI/4)
+    f(L/2, L/2, i, L/7)
 }
-
+j
 function f(x, y, r, d) {
   if (d > 3) {
     line(x, y, x += cos(r) * d, y -= sin(r) * d)
