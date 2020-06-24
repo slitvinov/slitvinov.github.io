87eed44e0cd72496dbf4aae975312c140d6244cf
Sergey Litvinov <slitvinov@gmail.com>
2020-06-24 12:49:24 +0200

    upd a.js

--- a.js
+++ a.js
@@ -5,20 +5,26 @@
  * each segment to the next is calculated with atan2() and the position of
  * the next is calculated with sin() and cos(). Based on code from Keith Peters.
  */
-var x = [],
-  y = [],
-  segNum = 20,
-  segLength = 18;
+var x = []
+var y = []
+var segNum = 20
+var segLength = 18
 
 for (var i = 0; i < segNum; i++) {
-  x[i] = 0;
-  y[i] = 0;
+  x[i] = 0
+  y[i] = 0
 }
 
 function setup() {
-  createCanvas(710, 400);
-  strokeWeight(9);
-  stroke(255, 100);
+    var cnv
+    cnv = createCanvas(windowWidth, windowHeight)
+    cnv.style('display', 'block')
+    strokeWeight(9)
+    stroke(255, 100)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
@@ -31,18 +37,21 @@ function draw() {
 }
 
 function dragSegment(i, xin, yin) {
-  var dx = xin - x[i];
-  var dy = yin - y[i];
-  var angle = atan2(dy, dx);
-  x[i] = xin - cos(angle) * segLength;
-  y[i] = yin - sin(angle) * segLength;
-  segment(x[i], y[i], angle);
+    var dx
+    var dy
+    var angle
+    dx = xin - x[i]
+    dy = yin - y[i]
+    angle = atan2(dy, dx)
+    x[i] = xin - cos(angle) * segLength
+    y[i] = yin - sin(angle) * segLength
+    segment(x[i], y[i], angle)
 }
 
 function segment(x, y, a) {
-  push();
-  translate(x, y);
-  rotate(a);
-  line(0, 0, segLength, 0);
-  pop();
+    push()
+    translate(x, y)
+    rotate(a)
+    line(0, 0, segLength, 0)
+    pop()
 }
